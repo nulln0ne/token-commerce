ee9003d4bcba9e4faa9656572edc5e78e3b40d09
nulln0ne <nulln0ne@protonmail.com>
2024-08-01 17:59:08 +0300

    backend: refactored strucrure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,33 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { PostgreModule } from './shared/postgre.module';
-import { RedisModule } from './shared/redis/redis.module';
-import { LoggingInterceptor } from './shared/logging.interceptor';
-import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
-import { AllExceptionsFilter } from './shared/http-exception.filter';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            isGlobal: true,
-            envFilePath: '/home/uladzislau/code/token-commerce/backend/src/.env',
-        }),
-        PostgreModule,
-        RedisModule,
-        UserModule,
-        AuthModule,
-    ],
-    providers: [
-        {
-            provide: APP_INTERCEPTOR,
-            useClass: LoggingInterceptor,
-        },
-        {
-            provide: APP_FILTER,
-            useClass: AllExceptionsFilter,
-        },
-    ],
+  imports: [UserModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,13 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserController } from './application/user.controller';
-import { UserService } from './domain/user.service';
-import { User } from './domain/user.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
-    controllers: [UserController],
-    providers: [UserService],
-    exports: [UserService],
+    controllers: [],
+    providers: [],
 })
 export class UserModule {}
